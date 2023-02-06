beaa9875572c8e17ff50ba81d6c9129cf6a87be1
JoseCP82 <josecamarap@gmail.com>
2023-02-06 09:45:49 +0100

    transactions service

--- src/main/java/bancaMach/backend/api_cashier_clients/CashierController.java
+++ src/main/java/bancaMach/backend/api_cashier_clients/CashierController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,12 +36,16 @@ public class CashierController {
     @GetMapping("/cashiers")
     public ResponseEntity<List<DTOCashier>> getAllCashiers(){
         List<DTOCashier> result = cashierService.getAllCashiers();
+        setCoordenates(result);
         return new ResponseEntity<>(result,new HttpHeaders(), HttpStatus.OK);
     }
 
     @GetMapping("/cashiers/{id}")
     public ResponseEntity<DTOCashier> getCashiertById(@PathVariable Long id){
         DTOCashier cashier = cashierService.getCashierById(id);
+        List<DTOCashier> aux = new ArrayList<>();
+        aux.add(cashier);
+        setCoordenates(aux);
         return new ResponseEntity<>(cashier,new HttpHeaders(), HttpStatus.OK);
     }
 
@@ -66,12 +71,14 @@ public class CashierController {
             @PathVariable Double lat,
             @PathVariable Double lng) {
         List<DTOCashier> result = cashierService.getAllCashiersByLoc(lat, lng);
+        setCoordenates(result);
         return new ResponseEntity<>(result,new HttpHeaders(), HttpStatus.OK);
     }
 
     @GetMapping("/geocashiers/{cp}")
     public ResponseEntity<List<DTOCashier>> getAllCashiersByLoc(@RequestBody DTORequestGeoCashier georeq, @PathVariable("cp") Integer cp){
         List<DTOCashier> result = cashierService.getAllCashiersByCP(cp);
+        setCoordenates(result);
         return new ResponseEntity<>(result,new HttpHeaders(), HttpStatus.OK);
     }
 
@@ -81,6 +88,14 @@ public class CashierController {
             @PathVariable Double lng,
             @PathVariable Integer distanceM){
         List<DTOCashier> result = cashierService.getAllCashiersByDistance(lat, lng, distanceM);
+        setCoordenates(result);
         return new ResponseEntity<>(result,new HttpHeaders(), HttpStatus.OK);
+    }
+
+    private void setCoordenates(List<DTOCashier> cashiers) {
+        for (DTOCashier c : cashiers) {
+            c.setLattitude(c.getPosition().getX());
+            c.setLongitude(c.getPosition().getY());
+        }
     }
 }
--- src/main/java/bancaMach/backend/api_cashier_services/TransactionService.java
+++ src/main/java/bancaMach/backend/api_cashier_services/TransactionService.java
@@ -1,6 +1,58 @@
 package bancaMach.backend.api_cashier_services;
 
+import bancaMach.backend.api_cahier_repositories.TransactionRepository;
+import bancaMach.backend.api_cashier_exceptions.RecordNotFoundException;
+import bancaMach.backend.api_cashier_models.DTOCashier;
+import bancaMach.backend.api_cashier_models.DTOTransaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TransactionService {
 
+    @Autowired
+    TransactionRepository transactionRepository;
 
+    @Autowired
+    ClientService clientService;
+
+    @Autowired
+    CashierService cashierService;
+
+    public DTOTransaction createOrUpdateTransaction(DTOTransaction transaction){
+        if (transaction.getId()!=null){
+            Optional<DTOTransaction> t = transactionRepository.findById(transaction.getId());
+            if (t.isPresent()){
+                transaction = transactionRepository.save(transaction);
+            }else{
+                throw new RecordNotFoundException("No transaction record exist for given id", transaction);
+            }
+        }else{
+            transaction = transactionRepository.save(transaction);
+        }
+        return transaction;
+    }
+
+    public List<DTOTransaction> getAllTransactions() { return transactionRepository.findAll(); }
+
+    public DTOTransaction getTransantionById(Long id){
+        Optional<DTOTransaction> transaction = transactionRepository.findById(id);
+        if (transaction.isPresent()){
+            return transaction.get();
+        }else{
+            throw new RecordNotFoundException("No transaction record exist for given id", id);
+        }
+    }
+
+    public void deleteTransactionById(Long id){
+        Optional<DTOTransaction> transaction = transactionRepository.findById(id);
+        if (transaction.isPresent()){
+            transactionRepository.deleteById(id);
+        }else{
+            throw new RecordNotFoundException("No transaction record exist for given id", id);
+        }
+    }
 }
