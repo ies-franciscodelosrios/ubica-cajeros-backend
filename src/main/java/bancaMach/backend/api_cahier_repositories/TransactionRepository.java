495e1572275396e0ba2b51e30048981b895ee645
JoseCP82 <josecamarap@gmail.com>
2023-03-07 12:26:48 +0100

    custom query in TransactionRepository

--- src/main/java/bancaMach/backend/api_cahier_repositories/TransactionRepository.java
+++ src/main/java/bancaMach/backend/api_cahier_repositories/TransactionRepository.java
@@ -2,8 +2,17 @@ package bancaMach.backend.api_cahier_repositories;
 
 import bancaMach.backend.api_cashier_models.dataobject.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query(
+            value = "SELECT * " +
+                    "FROM transaction " +
+                    "WHERE security_code ilike %:code% ;",
+            nativeQuery = true)
+    Transaction getTransactionByCode(@Param(value="code")String code);
 }
